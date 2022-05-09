f91e63c21b3fef91a202f12c05648a6a86b86f3c
ysercan28 <97476230+ysercan28@users.noreply.github.com>
2022-05-09 18:26:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ text = ""
 for sunular in os.listdir("sunular"):
     f = open("sunular/"+sunular, "rb")
     prs = Presentation(f)
-    print(sunular)
-    print("----------------------")
     for slide in prs.slides:
         for shape in slide.shapes:
             if hasattr(shape, "text"):
                 text += shape.text + "\n"
-print(text)
 exec(text)
